7664a307381cde851f97c5a5d54c89ab1310a4c0
zhangxiaoshang <zhangxiaoshang66@163.com>
2022-04-07 13:56:32 +0800

    temp

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -2,19 +2,19 @@ import { useState, useEffect } from 'react';
 import { Calendar, Spin } from 'antd';
 import moment from 'moment';
 import useProject from '@/hooks/useProject';
+import Header from '@/layouts/header';
+import Footer from '@/layouts/footer';
+import styles from './index.less';
 
 const HASHRATE_CONTRACT_ADDRESS = process.env.HASHRATE_CONTRACT_ADDRESS as string;
 
 export default () => {
-  const project = useProject();
-
-  useEffect(() => {
-    if (!project) return;
-
-    console.log(project.deliveryStart.format('YYYY.MM.DD HH:mm:ss'));
-  }, [project]);
+  const { project } = useProject();
 
   function dateCellRender(date: moment.Moment) {
+    if (!project) return null;
+
+    console.log(project);
     const today = moment().set({
       hour: 0,
       minute: 0,
@@ -33,11 +33,11 @@ export default () => {
       return '募集中';
     }
 
-    if (date.isSame(project?.startTime.clone().add(52, 'weeks'), 'day')) {
+    if (date.isSame(project?.startTime?.clone().add(52, 'weeks'), 'day')) {
       return '结束';
     }
 
-    if (date.isAfter(project?.startTime.clone().add(52, 'weeks'))) {
+    if (date.isAfter(project?.startTime?.clone().add(52, 'weeks'))) {
       return null;
     }
 
@@ -55,14 +55,38 @@ export default () => {
     console.log(date.format('YYYY.MM.DD HH:mm:ss'));
   };
 
-  return project ? (
-    <Calendar
-      validRange={[project?.startTime, project?.startTime.clone().add(52, 'weeks')]}
-      dateCellRender={dateCellRender}
-      disabledDate={(date: moment.Moment) => date.isBefore(project?.deliveryStart)}
-      onSelect={handleSelect}
-    />
-  ) : (
-    <Spin />
+  // return project ? (
+  //   <Calendar
+  //     validRange={[project?.startTime, project?.startTime.clone().add(52, 'weeks')]}
+  //     dateCellRender={dateCellRender}
+  //     disabledDate={(date: moment.Moment) => date.isBefore(project?.deliveryStart)}
+  //     onSelect={handleSelect}
+  //   />
+  // ) : (
+  //   <Spin />
+  // );
+
+  return (
+    <div>
+      <Header></Header>
+      <div className={styles.calendar}>
+        <div className={styles.sidebar}>
+          <div className="block">
+            <span className="label">Option account balance</span>
+            <span className="value">-</span>
+          </div>
+          <div className="block">
+            <span className="label">Deposit account balance</span>
+            <span className="value">{project?.depositAccountBalance} USDT</span>
+          </div>
+          <div className="block">
+            <span className="label">Initial Payment</span>
+            <span className="value">{project?.initialPayment} USDT</span>
+          </div>
+        </div>
+        <div className={styles.content}></div>
+      </div>
+      <Footer></Footer>
+    </div>
   );
 };
